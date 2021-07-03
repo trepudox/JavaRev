7494a646981696273ad06ab08805d88c53141cab
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-03 13:21:14 -0300

    commit inicial

---
+++ main/src/test/java/com/trepudox/main/TestTemplate.java
@@ -0,0 +1,13 @@
+package com.trepudox.main;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTemplate {
+
+    @Test
+    void test() {
+
+    }
+
+}
