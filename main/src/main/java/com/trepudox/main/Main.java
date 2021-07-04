48925d77f6c3bef0a2c3944c73dc4d87f58723f5
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-03 21:07:39 -0300

    teste

--- main/src/main/java/com/trepudox/main/Main.java
+++ main/src/main/java/com/trepudox/main/Main.java
@@ -1,10 +1,14 @@
 package com.trepudox.main;
 
+import com.trepudox.pessoa.Pessoa;
+import com.trepudox.pessoa.PessoaFactory;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        Pessoa p = PessoaFactory.create("Marco", 19);
+        System.out.println(p);
 
     }
 
