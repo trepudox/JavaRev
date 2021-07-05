0ab2ab7931b323ca37c1c40a6be8b4eebf0a1cae
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-05 02:22:07 -0300

    adicionando mais tipos de lambda

--- functional/src/main/java/com/trepudox/lambdas/Runner2.java
+++ functional/src/main/java/com/trepudox/lambdas/Runner2.java
@@ -1,12 +1,18 @@
 package com.trepudox.lambdas;
 
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
+import com.trepudox.pessoa.Pessoa;
+import com.trepudox.pessoa.PessoaFactory;
+
+import java.util.function.*;
 
 public class Runner2 {
 
     public static void main(String[] args) {
-        consumer();
+//        consumer();
+        supplier();
+//        predicate();
+//        function();
+//        biFunction();
 
     }
 
@@ -15,16 +21,32 @@ public class Runner2 {
         printaString.accept("STRING!");
     }
 
-    private static void predicate() {
+    private static void supplier() {
+        Supplier<Pessoa> criaMarco = () -> PessoaFactory.create("Marco", 19);
 
+        System.out.println(criaMarco.get());
+    }
+
+    private static void predicate() {
+        Predicate<String> stringMaiorQue5 = s -> s.length() > 5;
+        Predicate<String> stringIsNotEmpty = s -> !s.isEmpty();
+
+        System.out.println(stringMaiorQue5.and(stringIsNotEmpty).test("testee"));
+
+        System.out.println("Maçã: " + stringMaiorQue5.test("Maçã"));
+        System.out.println("Banana: " + stringMaiorQue5.test("Banana"));
     }
 
     private static void function() {
+        Function<String, Integer> stringEmInteger = Integer::valueOf;
 
+        System.out.println(stringEmInteger.apply("10") + 20);
     }
 
     private static void biFunction() {
         BiFunction<String, Integer, String> stringConcatNumero = (str, num) -> str + String.valueOf(num);
+
+        System.out.println(stringConcatNumero.apply("OI", 10));
     }
 
 }
