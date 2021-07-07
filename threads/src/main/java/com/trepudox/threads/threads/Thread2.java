54be86173021e16a893f0e0a5ab96f42c0d55ed6
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-07 00:23:31 -0300

    threads

---
+++ threads/src/main/java/com/trepudox/threads/threads/Thread2.java
@@ -0,0 +1,23 @@
+package com.trepudox.threads.threads;
+
+public class Thread2 implements Runnable {
+
+    private String name;
+
+    public Thread2(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        System.out.println("RODEI");
+        System.out.println(Thread.currentThread().getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
