03107618361c457013d5856d85e839416e87ef51
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-07 00:29:42 -0300

    orElse2

--- optional/src/main/java/com/trepudox/optional/Optional2.java
+++ optional/src/main/java/com/trepudox/optional/Optional2.java
@@ -6,7 +6,9 @@ public class Optional2 {
 
     public static void main(String[] args) {
 //        orElse();
-        filter();
+//        orElse2();
+//        filter();
+
     }
 
     private static void orElse() {
@@ -15,12 +17,22 @@ public class Optional2 {
         System.out.println(optNull.orElse("aaaa"));
     }
 
+    private static void orElse2() {
+        Optional<String> optional = Optional.ofNullable(retornaValor());
+
+        System.out.println(optional.orElse("NULO"));
+    }
+
     private static void filter() {
         Optional<String> optMarco = Optional.of("Marco");
 
         System.out.println(
             optMarco.filter(value -> value.startsWith("a")).isPresent()
         );
+    }
+
+    private static String retornaValor() {
+        return System.currentTimeMillis() % 2 == 0 ? null : "valor";
     }
 
 }
