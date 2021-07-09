a061112c5569f7b3b0eee9847b3cdb53fb4e516b
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-09 14:09:58 -0300

    array list

---
+++ collections/src/main/java/com/trepudox/list/ArrayListRev.java
@@ -0,0 +1,14 @@
+package com.trepudox.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListRev {
+
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>(List.of("aa", "bb"));
+
+        System.out.println(arrayList);
+    }
+
+}
