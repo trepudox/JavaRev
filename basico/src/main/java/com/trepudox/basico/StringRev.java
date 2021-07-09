85883a99c938cc960d963d98be7b3299a3b11399
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-09 16:11:27 -0300

    testes basicos

--- basico/src/main/java/com/trepudox/basico/StringRev.java
+++ basico/src/main/java/com/trepudox/basico/StringRev.java
@@ -4,14 +4,20 @@ public class StringRev {
 
     public static void main(String[] args) {
 
-        stringSubstring();
+        stringSubstring("1234567891011121");
+        isFirstCharacterUpper("Teste");
 
     }
 
-    private static void stringSubstring() {
-        String str = "1234567891011121";
-
+    private static void stringSubstring(String str) {
         System.out.println(str.substring(str.length() - 4));
+    }
+
+    private static void isFirstCharacterUpper(String str) {
+        System.out.println(
+                Character.isUpperCase(str.charAt(0))
+        );
+
     }
 
 }
