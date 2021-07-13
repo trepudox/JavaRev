b9c5f020208b35131410a685430ddfe6bc826b3a
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-13 13:57:25 -0300

    operators

--- projectreactor/src/test/java/com/trepudox/flux/FluxSimpleOperators.java
+++ projectreactor/src/test/java/com/trepudox/flux/FluxSimpleOperators.java
@@ -10,32 +10,6 @@ import java.time.Duration;
 @Slf4j
 public class FluxSimpleOperators {
 
-    @Test
-    void defaultIfEmpty() {
-        // se o fluxo estiver vazio, retorna o objeto passado por parâmetro
-
-        Flux<Object> flux = Flux.empty()
-                .defaultIfEmpty(0)
-                .log();
-
-        flux.subscribe();
-    }
-
-    @Test
-    void repeatAndDelayElements() throws Exception {
-        // delayElements cria um intervalo entre os elementos
-        // repeat faz com o fluxo se repita, seja infinitamente, uma quantidade delimitada ou até bater uma condição booleana
-
-        Flux<Integer> flux = Flux.just(1, 2, 3)
-                .delayElements(Duration.ofSeconds(1))
-                .repeat(3)
-                .log();
-
-        flux.subscribe();
-
-        Thread.sleep(10000);
-    }
-
     @Nested
     class MergeAndConcat {
 
@@ -67,7 +41,6 @@ public class FluxSimpleOperators {
 
                     return Flux.just(e);
                 })
-                        .onErrorContinue((ex, o) -> System.out.println(ex.getMessage() + " causado por: " + o))
                         .delayElements(Duration.ofMillis(50));
 
                 Flux<String> mergedFlux = Flux.mergeDelayError(1, fluxLetras, fluxNums)
@@ -97,18 +70,58 @@ public class FluxSimpleOperators {
 
             @Test
             void concatDelayError() throws Exception {
-                // mergeDelayError fará o merge dos Flux, trabalhando de maneira ASSÍNCRONA, se houver algum erro
+                // concatDelayError fará o concat dos Flux, trabalhando de maneira SSÍNCRONA, se houver algum erro
                 // durante a execução de algum dos Flux, a exceção será lançada apenas no final de toda a emissão dos
                 // elementos
 
                 Flux<String> fluxLetras = Flux.just("A", "B", "C", "D", "E").delayElements(Duration.ofMillis(50));
-                Flux<String> fluxNums = Flux.just("1", "2", "3", "4", "5").delayElements(Duration.ofMillis(50));
+                Flux<String> fluxNums = Flux.just("1", "2", "3", "4", "5").flatMap(e -> {
+                    if (e.equalsIgnoreCase("3"))
+                        throw new RuntimeException("ERRO!");
 
-                Flux.concatDelayError(fluxLetras, fluxNums).log().subscribe();
+                    return Flux.just(e);
+                })
+                        .delayElements(Duration.ofMillis(50));
 
-                Thread.sleep(1000);
+                Flux<String> mergedFlux = Flux.concatDelayError(fluxLetras, fluxNums)
+                        .log();
+
+                mergedFlux.subscribe();
+
+                Thread.sleep(1500);
             }
 
+        }
+
+    }
+
+    @Nested
+    class OtherOperators {
+
+        @Test
+        void defaultIfEmpty() {
+            // se o fluxo estiver vazio, retorna o objeto passado por parâmetro
+
+            Flux<Object> flux = Flux.empty()
+                    .defaultIfEmpty(0)
+                    .log();
+
+            flux.subscribe();
+        }
+
+        @Test
+        void repeatAndDelayElements() throws Exception {
+            // delayElements cria um intervalo entre os elementos
+            // repeat faz com o fluxo se repita, seja infinitamente, uma quantidade delimitada ou até bater uma condição booleana
+
+            Flux<Integer> flux = Flux.just(1, 2, 3)
+                    .delayElements(Duration.ofSeconds(1))
+                    .repeat(3)
+                    .log();
+
+            flux.subscribe();
+
+            Thread.sleep(10000);
         }
 
     }
