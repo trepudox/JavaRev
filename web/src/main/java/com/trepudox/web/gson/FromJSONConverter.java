fa6af0d42bf82196e2d32bf38b920ae877e4e849
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-07-21 13:22:30 -0300

    adicionando timeout

--- web/src/main/java/com/trepudox/web/gson/FromJSONConverter.java
+++ web/src/main/java/com/trepudox/web/gson/FromJSONConverter.java
@@ -8,6 +8,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
 public class FromJSONConverter {
 
@@ -19,18 +20,27 @@ public class FromJSONConverter {
         String stringUri = "http://localhost:8080/pessoas/all";
         URI uri = URI.create(stringUri);
 
-        HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+        HttpRequest request = HttpRequest.newBuilder(uri)
+                .GET()
+                .timeout(Duration.ofMillis(1000))
+                .build();
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String stringResponse = response.body();
+        long inicio = System.currentTimeMillis();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String stringResponse = response.body();
 
-        JsonArray jsonArray = (JsonArray) JsonParser.parseString(stringResponse);
+            JsonArray jsonArray = (JsonArray) JsonParser.parseString(stringResponse);
 
-        for (JsonElement jsonElement : jsonArray) {
-            PessoaModel pessoaModel = gson.fromJson(jsonElement, PessoaModel.class);
-
-            System.out.println(pessoaModel);
+            for (JsonElement jsonElement : jsonArray) {
+                PessoaModel pessoaModel = gson.fromJson(jsonElement, PessoaModel.class);
+                System.out.println(pessoaModel);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo em ms: " + (fim - inicio));
 
     }
 
