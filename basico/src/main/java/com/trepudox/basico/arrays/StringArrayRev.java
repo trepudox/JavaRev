461ec3caf4d389f0366c009220ff6ee9b1b1d036
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-08-05 14:12:13 -0300

    refatorando

--- basico/src/main/java/com/trepudox/basico/arrays/StringArrayRev.java
+++ basico/src/main/java/com/trepudox/basico/arrays/StringArrayRev.java
@@ -5,23 +5,23 @@ import java.util.Arrays;
 public class StringArrayRev {
 
     public static void main(String[] args) {
-        String[] stringArray = {"marco", "string", "java"};
-        String[] cloneArray = stringArray.clone();
+        String[] originalStringArray = {"marco", "string", "java"};
+        String[] clonedStringArray = originalStringArray.clone();
 
-        stringArray[1] = "marco2?";
+        originalStringArray[1] = "marco2?";
 
-        System.out.println("clone: " + Arrays.toString(cloneArray));
-        System.out.println("original: " + Arrays.toString(stringArray));
+        System.out.println("clone: " + Arrays.toString(clonedStringArray));
+        System.out.println("original: " + Arrays.toString(originalStringArray));
 
         String value = "marco";
         boolean contains = false;
-        for (String s : stringArray) {
+        for (String s : originalStringArray) {
             if (s.equals(value)) {
                 contains = true;
                 break;
             }
         }
-        System.out.println("stringArray contains '" + value + "': " + contains);
+        System.out.println("originalStringArray contains '" + value + "': " + contains);
     }
 
 }
