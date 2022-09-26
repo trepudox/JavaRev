1244f0ca5176e27d04c8bfdd7aca48f2aea7c077
Marco Aurelio Queiroz <teoaa2@gmail.com>
2022-09-26 18:59:26 -0300

    commit teste token github

--- datas/src/main/java/com/trepudox/datas/instant/InstantRev.java
+++ datas/src/main/java/com/trepudox/datas/instant/InstantRev.java
@@ -6,10 +6,9 @@ public class InstantRev {
 
     public static void main(String[] args) {
         Instant instant = Instant.now();
+        System.out.println(instant);
 
         System.out.println(Instant.from(ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("America/Sao_Paulo"))));
-
-        System.out.println(instant);
 
         /*
         * Convertendo um epoch timestamp num Instant e convertendo o Instant para LocalDateTime
